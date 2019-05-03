3160909894f9e4f145f742a867a621299ef22fc3
lsz <46678005+lszanata@users.noreply.github.com>
2019-05-03 21:35:22 +0000

    Initial commit

---
+++ climate.spec.js
@@ -0,0 +1,26 @@
+// const climate = require( './climate.js' );
+
+// jest.mock( './climate' );
+
+// const reply = {
+//   location: 'MOCK NAME, MOCK REGION, MOCK COUNTRY',
+//     coordinates: {
+//         latitude: 0.0,
+//         longitude: 0.0
+//     },
+//     temperature: {
+//         current: 5000.0,
+//         apparent: 5000.1,
+//         min: 0.0,
+//         max: 9999.9
+//     }
+// };
+
+// it( 'testing my mock API call', () => {
+//   // expect.assertions( 1 );
+//   climate.getTemperature( 'mocking' ).then( data => expect( data ).toEqual( reply ) );
+// });
+
+// // test( 'tesing mock with "test"', () => {
+// //   expect( climate.getTemperature( 'mocking' )).resolves.toEqual( reply );
+// // });
