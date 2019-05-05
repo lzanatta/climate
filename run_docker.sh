04fef64e6e5c8eea5bc259cde7816e822aa0fcb5
Stéfano Stypulkowski Zanata <made.in.stefano@gmail.com>
2019-05-05 21:46:31 +0100

    Fixing nock tests; Fixing tests scripts

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker run --rm -it \
-    -e NPM_TOKEN="$NPM_TOKEN" \
-    -v `pwd`:/app/ \
-    -w /app/ \
+  -e NPM_TOKEN="$NPM_TOKEN" \
+  -v `pwd`:/app/ \
+  -w /app/ \
     node:12.1.0-alpine /bin/sh
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -5,4 +5,5 @@ set -e
 docker run --rm -t \
   -v `pwd`:/app/ \
   -w /app/ \
-    node:12.1.0-alpine /bin/sh -c "npm install && npm run test"
+    node:12.1.0-alpine \
+    /bin/sh -c "npm install && npm run lint && npm run test"
